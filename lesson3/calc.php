3b85da41e2e54e5b3cab9291c3b7c5503652b464
Semyonov Alexandr <alexandrbublik@gmail.com>
2019-08-15 15:55:32 +0300

    15 08 2019 15 55

--- lesson3/calc.php
+++ lesson3/calc.php
@@ -2,7 +2,9 @@
 
   var_dump ($_GET);
   echo '<br>';
-  echo $res =  implode($_GET);
+  $res = ($_GET['num1'] . $_GET['operation'] . $_GET['num2']);
+  $res = eval($res);
+  echo $res;
   echo '<br>';
   echo $_GET['num1'] . ' ' . $_GET['operation'] . ' ' . $_GET['num2'] . ' = ';
   echo '<br>';
