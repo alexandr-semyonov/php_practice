42af5c9fbe78b9a28202ca7da982064e735330e3
Semyonov Alexandr <alexandrbublik@gmail.com>
2019-08-16 12:49:21 +0300

    leran file functions

--- lesson4/index.php
+++ lesson4/index.php
@@ -5,10 +5,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>File operaions</title>
+  
 </head>
 <body>
-  <h1>Hello</h1>
+  <h1>Hello</h1>  
   <?php
+
 
     $path = __DIR__ . '/data.txt';
     $fh = fopen($path, 'w');
