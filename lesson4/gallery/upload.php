b397644383a9007837aed682d0aa7d9fb2fc327a
alexandr-semyonov <alexandrbublik@gmail.com>
2019-08-21 23:13:07 +0300

    Закончил геллерею, осталось ограничение типа файла

--- lesson4/gallery/upload.php
+++ lesson4/gallery/upload.php
@@ -1,34 +1,19 @@
 <?php
-
 if ( isset($_FILES['myimg']) ) {
     if (0 == $_FILES['myimg']['error']) {
-        $fileName = $_FILES['myimg']['name'];
-        $arrOfNames = scandir( __DIR__ . '/img' );
-        
+        $uploadFile = $_FILES['myimg']['name'];
+        $fileName = $uploadFile;
 
-
-        if (
-            in_array($_FILES['myimg']['name'], $arrOfNames)
-            &&
-            strstr($_FILES['myimg']['name'], '(copy)')
-            ){
-            $fileName = explode('.', $fileName);
-            $fileNameArr = explode('(copy)',$fileName[0]);
-            $fileNameArr[1] = 2;
-            $fileName[0] = implode('(copy-', $fileNameArr) . ')';
-            $fileName = implode('.',$fileName);
-        }    
-        
-        if (in_array($_FILES['myimg']['name'], $arrOfNames)){
-            $fileName = explode('.', $fileName);
-            $fileName[0] = $fileName[0] . '(copy)';
+        if (file_exists( __DIR__ . '/img' . '/' . $fileName )){
+            $fileName = explode('.',$fileName);
+            $fileName[0] = $fileName[0] . 'x' .rand(0, 999);
             $fileName = implode('.', $fileName);
-        }    
-
+        }
         move_uploaded_file(
         $_FILES['myimg']['tmp_name'],
         __DIR__ . '/img' . '/' . $fileName
         );
-        echo $_FILES['myimg']['name'];
     }
 }
+
+header('location:index.php');
