e2a938aa91930d9789e0463774464b215dd5c770
alexandr-semyonov <alexandrbublik@gmail.com>
2019-08-22 11:05:12 +0300

    Добавил проверку типа файла

---
+++ lesson4/finfo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Finfo</title>
+</head>
+<body>
+    <?php
+        $finfo = finfo_open(FILEINFO_NONE);
+        $info = finfo_file($finfo ,__DIR__ . '/1000.jpg');
+        echo $info;
+        finfo_close($finfo);
+        echo '<br>';
+        $finfo = finfo_open(FILEINFO_NONE);
+        $info = finfo_file($finfo ,__DIR__ . '/screen.png');
+        echo $info;
+        finfo_close($finfo);
+    ?>
+</body>
+</html>
--- lesson4/gallery/upload.php
+++ lesson4/gallery/upload.php
@@ -1,19 +1,28 @@
 <?php
 if ( isset($_FILES['myimg']) ) {
     if (0 == $_FILES['myimg']['error']) {
-        $uploadFile = $_FILES['myimg']['name'];
-        $fileName = $uploadFile;
-
-        if (file_exists( __DIR__ . '/img' . '/' . $fileName )){
-            $fileName = explode('.',$fileName);
-            $fileName[0] = $fileName[0] . 'x' .rand(0, 999);
-            $fileName = implode('.', $fileName);
-        }
-        move_uploaded_file(
-        $_FILES['myimg']['tmp_name'],
-        __DIR__ . '/img' . '/' . $fileName
-        );
+        $uploadedFile = $_FILES['myimg']['tmp_name'];
+        $finfo = finfo_open(FILEINFO_NONE);
+        $info = finfo_file($finfo , $uploadedFile);
+        finfo_close($finfo);
+        $infoArr = explode(',' , $info);
+        if ('JPEG image data' == $infoArr[0] || 'PNG image data' == $infoArr[0] ){
+            $fileName = $_FILES['myimg']['name'];        
+            if (file_exists( __DIR__ . '/img' . '/' . $fileName )){
+                $fileName = explode('.',$fileName);
+                $fileName[0] = $fileName[0] . 'x' .rand(0, 999);
+                $fileName = implode('.', $fileName);
+            }
+            move_uploaded_file(
+            $_FILES['myimg']['tmp_name'],
+            __DIR__ . '/img' . '/' . $fileName
+            );
+        } else {
+            exit (
+            '<h2>Invalid file format. Use JPEG or PNG.</h2>
+            <br>
+            <a href="index.php">Back to gallery</a>');
+        }       
     }
 }
-
 header('location:index.php');
