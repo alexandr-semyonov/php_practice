6131fe6b4170a7262546e06b721defb250fb5385
Alexandr Semyonov <alexandrbublik@gmail.com>
2019-09-04 00:35:19 +0300

    Добавил два класса

---
+++ lesson6/Gallery/Classes/User.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class User
+{
+  protected $userName;
+  protected $password;
+
+  public function __construct($userName, $password)
+  {
+    $this->userName = $userName;
+    $this->password = $password;
+  }
+
+  public function getUserName()
+  {
+    return $this->userName;
+  }
+
+  public function getPassword()
+  {
+    return $this->password;
+  }
+}
