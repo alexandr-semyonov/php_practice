47cda721e25d17fbdd72bdbbe9d2fb6e583c6aee
Semyonov Alexandr <alexandrbublik@gmail.com>
2019-09-25 12:05:06 +0300

    Start lesson 9

--- lesson8/install.php
+++ lesson8/install.php
@@ -13,6 +13,6 @@ var_dump($db->execute('CREATE TABLE news (
 var_dump($db->execute("INSERT INTO news ( заголовок, текст, автор) 
   VALUES ( 'Новость 1', 'Name of news 1 |Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto eligendi aliquid sapiente exercitationem corporis ex, culpa voluptatibus at voluptatem. Tempore qui sit velit laborum veritatis, illum repellat est culpa odit', 'Александр')" ));
 var_dump($db->execute("INSERT INTO news ( заголовок, текст, автор) 
-  VALUES ( 'Новость 2', 'Name of news 2 |Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto eligendi aliquid sapiente exercitationem corporis ex, culpa voluptatibus at voluptatem. Tempore qui sit velit laborum veritatis, illum repellat est culpa odit', 'Александр')" ));
+  VALUES ( 'Новость 2', 'Name of news 2 |Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto eligendi aliquid sapiente exercitationem corporis ex, culpa voluptatibus at voluptatem. Tempore qui sit velit laborum veritatis, illum repellat est culpa odit', 'Петр')" ));
 var_dump($db->execute("INSERT INTO news ( заголовок, текст, автор) 
-  VALUES ( 'Новость 3', 'Name of news 3 |Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto eligendi aliquid sapiente exercitationem corporis ex, culpa voluptatibus at voluptatem. Tempore qui sit velit laborum veritatis, illum repellat est culpa odit', 'Александр')" ));
+  VALUES ( 'Новость 3', 'Name of news 3 |Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto eligendi aliquid sapiente exercitationem corporis ex, culpa voluptatibus at voluptatem. Tempore qui sit velit laborum veritatis, illum repellat est culpa odit', 'Валера')" ));
