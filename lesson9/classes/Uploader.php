e425054c34adbf4f1512a638d97e9915de0e1740
alexandr-semyonov <alexandrbublik@gmail.com>
2019-09-27 17:50:48 +0300

    27 09 2019 17 47

---
+++ lesson9/classes/Uploader.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__ . '/Db.php';
+
+class Uploader
+{
+    public function __construct($fieldName)
+    {
+        $this->fieldName = $fieldName;
+    }
+    public function isUploaded()
+    {
+        return isset($_FILES[$this->fieldName]['name']);
+    }
+    public function upload( $uploadDir, $filename)
+    {
+        if ($this->isUploaded()){
+            move_uploaded_file(
+                $_FILES[$this->fieldName]['tmp_name'],
+                $uploadDir . $filename
+            );
+          $db = new Db;
+          $sql = 'SELECT * FROM gallery';
+        }
+    }
+    protected $fieldName;
+}
