fd736aa761c23b60427da105c6fbd03b62430db2
Alexandr Semyonov <alexandrbublik@gmail.com>
2019-10-11 21:28:59 +0300

    11 10 2019 21 28

--- lesson9/classes/View.php
+++ lesson9/classes/View.php
@@ -3,8 +3,7 @@
 
 class View
 {
-  protected $data = [],
-    $template;
+  protected $data = [], $template;
 
   public function assign(string $name, $value)
   {
@@ -15,7 +14,9 @@ class View
   public function render(string $template)
   {
     ob_start();
+    require_once __DIR__ . '/../templates/header.php';
     require_once __DIR__ . './../templates/' . $template . '.php';
+    require_once __DIR__ . '/../templates/footer.php';
     $this->template = ob_get_clean();
     return $this;
   }
--- lesson9/js/main.js
+++ lesson9/js/main.js
@@ -1,4 +1,15 @@
-
 $( document ).ready(function() {
     $('.alertButton').click(function(){alert("Hello! I am an alert box!")} );
+});
+
+$( document ).ready(function() {
+    let location = window.location.href;
+    let cur_url = '/' + location.split('/').pop();
+    $('.nav-item').each(function () {
+        let link = $(this).find('a').attr('href');
+        if (cur_url == link) {
+           $(this).addClass('active');
+           $(this).find('a').addClass('disabled');
+        }
+    });
 });
